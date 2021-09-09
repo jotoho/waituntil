090616d05d81c8b52d6a1fb2eec16e3c4f9ad452
Jonas Tobias Hopusch <git@jotoho.de>
2021-09-09 23:09:26 +0200

    Move functions into separate kotlin files

---
+++ app/src/main/kotlin/de/jotoho/waituntil/sleeping.kt
@@ -0,0 +1,25 @@
+package de.jotoho.waituntil
+
+import java.lang.Math
+import java.time.Instant
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.ChronoUnit
+import java.util.TimeZone
+
+public fun waitUntilTimeStamp(timestamp: ZonedDateTime) {
+    Thread.sleep(Math.max(Instant.now().until(timestamp, ChronoUnit.MILLIS), 0))
+
+    val formattedTimeStamp: String =
+            DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG)
+                    .withZone(TimeZone.getDefault().toZoneId())
+                    .format(Instant.now())
+
+    when (applicationOutputLanguage) {
+        langGerman -> System.err.println("Erfolgreich bis $formattedTimeStamp gewartet!")
+        else -> {
+            System.err.println("Successfully waited until $formattedTimeStamp")
+        }
+    }
+}
--- app/src/main/kotlin/de/jotoho/waituntil/start.kt
+++ app/src/main/kotlin/de/jotoho/waituntil/start.kt
@@ -2,13 +2,15 @@ package de.jotoho.waituntil
 
 import java.util.Locale
 import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 import java.util.TimeZone
 import java.time.Instant
 import java.time.LocalTime
 import java.time.LocalDate
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
+
+import de.jotoho.waituntil.waitUntilTimeStamp
+import de.jotoho.waituntil.calculateAndAnnounceTargetTime
 
 // This file contains the main function and other utility function necessary for interpreting the terminal arguments.
 // See README.md and LICENSE.md for license information
@@ -18,43 +20,6 @@ val langGerman: String = Locale.GERMAN.getLanguage();
 val applicationOutputLanguage: String = if (Locale.getDefault().getLanguage().equals(Locale.GERMAN.getLanguage()))
                                     Locale.GERMAN.getLanguage()
                                     else Locale.ENGLISH.getLanguage();
-
-fun waitUntilTimeStamp(timestamp: ZonedDateTime) {
-    Thread.sleep(
-        Math.max(Instant.now().until(timestamp, ChronoUnit.MILLIS), 0)
-        );
-
-    val formattedTimeStamp: String = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG)
-                                        .withZone(TimeZone.getDefault().toZoneId())
-                                        .format(Instant.now());
-
-    when (applicationOutputLanguage) {
-        langGerman -> System.err.println("Erfolgreich bis $formattedTimeStamp gewartet!");
-        else -> {
-            System.err.println("Successfully waited until $formattedTimeStamp");
-        }
-    }
-}
-
-fun calculateAndAnnounceTargetTime(userTimeInputRaw: String): ZonedDateTime {
-    val userTimeInputRelative = LocalTime.parse(userTimeInputRaw);
-    val userTimeInputAbsolute = ZonedDateTime.of(LocalDate.now(), userTimeInputRelative, TimeZone.getDefault().toZoneId());
-
-    val userTimeInputFinal = if (Instant.now().isBefore(userTimeInputAbsolute.toInstant())) userTimeInputAbsolute else userTimeInputAbsolute.plusDays(1);
-
-    val formattedTimeStamp = userTimeInputFinal.format(
-        DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG)
-    );
-
-    when (applicationOutputLanguage) {
-        langGerman -> System.err.println("Dieses Program wird bis zum $formattedTimeStamp warten.");
-        else -> {
-            println("WaitUntil will suspend until $formattedTimeStamp");
-        }
-    }
-
-    return userTimeInputFinal;
-}
 
 fun main(args: Array<String>) {
     val optionDictionary = mapOf(Pair("-h", "--help"));
