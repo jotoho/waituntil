88b0acf69829d4b56d8b3ede02f5d805ef8080ab
Jonas Tobias Hopusch <git@jotoho.de>
2021-12-01 14:48:27 +0100

    Delete obsolete compile and run scripts

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# shellcheck disable=SC2046
-# Word splitting in find results is intentional!
-kotlinc $(find src/main -type f -iname '*.kt') -jvm-target 17 -include-runtime -d waituntil.jar
