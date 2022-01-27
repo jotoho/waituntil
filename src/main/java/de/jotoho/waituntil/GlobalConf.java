bb0ef7631bf91480c530179e82fc19b2be1ba578
Jonas Tobias Hopusch <git@jotoho.de>
2022-01-28 00:31:59 +0100

    Add copyright banner to all java sourcefiles
    For the purposes of transparency regarding the FOSS license (AGPLv3+) used by this project,
    all source files should contain the standard license header.

--- src/main/java/de/jotoho/waituntil/GlobalConf.java
+++ src/main/java/de/jotoho/waituntil/GlobalConf.java
@@ -1,5 +1,23 @@
 package de.jotoho.waituntil;
 
+/*
+    waituntil - a tool for delaying command execution until the specified time
+    Copyright (C) 2022 Jonas Tobias Hopusch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import java.util.Locale;
 
 public record GlobalConf() {
