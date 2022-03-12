ddf8047ebd92b009b4990023b2f2b6b2f53bb6c0
Jonas Tobias Hopusch <git@jotoho.de>
2022-03-12 03:13:30 +0100

    Disable modularization
    The modularization file was heavily interfering with the addition and usage of the commons-cli
    dependency which does not have a static module name.
    
    Since the benefits it gave this very small application were miniscule, I have decided to
    delete / comment out all relevant configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ tasks.jar {
         attributes(
             "Implementation-Title" to project.name,
             "Implementation-Version" to project.version,
-            "Main-Class" to "de.jotoho.waituntil.Main",
-            "Main-Module" to "de.jotoho.waituntil.main"
+            "Main-Class" to "de.jotoho.waituntil.Main"
+            //"Main-Module" to "de.jotoho.waituntil.main"
         )
     }
 }
@@ -49,5 +49,5 @@ tasks.jar {
 application {
     // Define the main class for the application.
     mainClass.set("de.jotoho.waituntil.Main")
-    mainModule.set("de.jotoho.waituntil.main")
+    //mainModule.set("de.jotoho.waituntil.main")
 }
--- src/main/java/module-info.java
+++
@@ -1,2 +0,0 @@
-module de.jotoho.waituntil.main {
-}
