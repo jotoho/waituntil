b53e7a617d88f7ec512f8e7befb2aca300ed48ea
Jonas Tobias Hopusch <git@jotoho.de>
2022-03-12 03:16:00 +0100

    Refactor main method to use commons-cli to parse arguments

---
+++ src/main/java/de/jotoho/waituntil/AppOptions.java
@@ -0,0 +1,23 @@
+package de.jotoho.waituntil;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+final class AppOptions {
+    // Disable Instance Creation
+    private AppOptions() {}
+
+    public final static Option help =
+        Option.builder().argName("h").longOpt("help").desc("Shows this help " +
+            "message and exits").build();
+    public final static Option version =
+        Option.builder().argName("v").longOpt("version").desc("Shows version information and exits").build();
+
+    private final static Options options = new Options()
+                                            .addOption(help)
+                                            .addOption(version);
+
+    public static Options getOptions() {
+        return options;
+    }
+}
